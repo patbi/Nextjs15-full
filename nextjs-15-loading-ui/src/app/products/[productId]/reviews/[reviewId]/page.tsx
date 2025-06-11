503455682a261b122b0f045680a24e41b2783d30
Patrick <biyagapatrick@gmail.com>
2025-06-11 21:24:30 +0100

    Next.js-15 Learning In Progress...

---
+++ nextjs-15-loading-ui/src/app/products/[productId]/reviews/[reviewId]/page.tsx
@@ -0,0 +1,12 @@
+import { notFound, redirect } from "next/navigation";
+
+export default async function ProductReview({ params }: {
+	params: Promise<{ productId: string; reviewId: string }>;
+}) {
+	const { productId, reviewId } = await params;
+	if (parseInt(reviewId) > 1000) {
+		//notFound();
+		redirect("/products");
+	}
+	return <h1>Review {reviewId} for product {productId} </h1>;
+}
